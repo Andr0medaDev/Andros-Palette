74e216a698067afe2e02e75587a6bb09a9d762db
Andr0med4modz <wdc7211@gmail.com>
2025-11-08 13:47:01 -0500

    testttttttt #3

--- src/main/java/top/andro/a_palette/blockentity/ArtPaletteBlockEntity.java
+++ src/main/java/top/andro/a_palette/blockentity/ArtPaletteBlockEntity.java
@@ -1,24 +1,4 @@
-package top.andro.apalette.blockentity;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.Container;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+package top.andro.a_palette.blockentity;
 
 /*public class ArtPaletteBlockEntity extends BlockEntity implements MenuProvider {
     private final ItemStackHandler itemHandler = new ItemStackHandler(4);
